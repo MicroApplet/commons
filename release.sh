83c26172e2bec5bcab9004c6ac5577adaf33c35b
Asial Jim <asialjim@hotmail.com>
2025-03-19 14:47:21 +0800

    init

--- release.sh
+++ release.sh
@@ -1,14 +1,8 @@
-#!/bin/bash
-
 # 定义版本号生成规则（示例：当前版本为 1.0.0-SNAPSHOT，release 版本为 1.0.0，新快照版本为 1.0.1-SNAPSHOT）
 CURRENT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 RELEASE_VERSION=${CURRENT_VERSION%-SNAPSHOT}
 NEW_VERSION=$(echo $RELEASE_VERSION | awk -F. '{$NF = $NF + 1; print $0}' | sed 's/ /./g')-SNAPSHOT
 
 # 执行自动化发布
-mvn clean release:prepare -B \
-  -DreleaseVersion=$RELEASE_VERSION \
-  -DdevelopmentVersion=$NEW_VERSION \
-  -Dtag=v$RELEASE_VERSION
-
+mvn clean release:prepare -B -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$NEW_VERSION -DasialjimVersion=$RELEASE_VERSION -P release,!dev
 mvn release:perform -B
